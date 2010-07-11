a3ef62e3783e5fefdda3cf0fb9dd454c2a08745a
Jao Balao <jaobalao@gmail.com>
2010-07-10 22:39:12 -0500

    Completed half of chapeter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,11 @@
 #  updated_at :datetime
 #
 
+require 'digest'
+
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -19,6 +22,41 @@ class User < ActiveRecord::Base
   validates_length_of   :name, :maximum =>50
   validates_format_of   :email, :with => EmailRegex
   validates_uniqueness_of :email, :case_sensitive => false
+
+  # Automatically create the virtual attribute 'password_confirmation'.
+  validates_confirmation_of :password
+
+  # Password validations.
+  validates_presence_of :password
+  validates_length_of   :password, :within => 6..40
+
+  before_save :encrypt_password
+
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    # Compare encrypted_password with the encrypted version of
+    # submitted_password.
+    encrypted_password == encrypt(submitted_password)
+  end
+
+ private
+
+    def encrypt_password
+      self.salt = make_salt
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{salt}#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}#{password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 
 end
 
